2df5a3d4f03da0edb7dc285144373e5ec5b1e878
Jorge Diaz <jorge7soccer@gmail.com>
2018-06-14 09:31:52 -0400

    ENH: Added documentation to the functions.

--- exercises/04-exercise.py
+++ exercises/04-exercise.py
@@ -17,6 +17,10 @@ adc2counts = lambda x: ((int(x, 16) >> 8) - 0x40000) * fc \
 enc2counts = lambda x: int(x) if int(x) <= 0 else -(int(x) ^ 0xffffff - 1)
 
 def raw_read_in(raw_files):
+	'''
+	the raw files to read in
+	Returns a list of raw files read in as arrays
+	'''
 	raw_files_read_in = []
 	index = 0
 	for file in raw_files:
@@ -26,9 +30,18 @@ def raw_read_in(raw_files):
 	return raw_files_read_in
 
 def files_in_dir(raw_files_read_in):
+	'''
+	returns the list of raw files read in, that is, the data for 
+	each file is stored in an index of a ndarray
+	returns a numpy array
+	'''
 	return np.asarray(raw_files_read_in)
 
 def print_size_files(file_read_in_dir, raw_files):
+	'''
+	prints how many rows and how many columns there are for 
+	each raw file read in
+	'''
 	for index in range(len(file_read_in_dir)):
 		print(str(raw_files[index]) + " - (Rows, Columns) - "+ str(file_read_in_dir[index].shape))
 
@@ -37,16 +50,17 @@ def total_time(an_files_only, directory_string):
 	adding the sum of the time_in_sec AND time_in_nanosec of each ROW to a list
 	once the list reaches the last index, the list is stored in another list that
 	will hold the sum of time, from each row, of x file
+	returns a list of lists, the lists in each index contain the 
+	totaltime of that specific file , 
+	e.g.: time2totaltime[0] = list of totaltime for each row from file associated with this specific index
 	'''
-	outer_index = 0
-	inner_index = 0
 	time = 0.0
 	temp_total_time = []
 	time2totaltime = []
 
-	for file in an_files_only:
+	for outer_index in range(len(an_files_only)):
 		read_an_file = np.loadtxt(directory_string + "/" + an_files_only[outer_index])
-		while inner_index < read_an_file.shape[0]: # less than number of rows in file
+		for inner_index in range(read_an_file.shape[0]): # less than number of rows in file
 			if inner_index != len(read_an_file): # if inner index != the length or num of elements of file
 				time_in_sec = read_an_file[inner_index][0] #gets time in sec
 				time_in_nanosec = read_an_file[inner_index][1]#gets time for nanosec
@@ -54,13 +68,15 @@ def total_time(an_files_only, directory_string):
 				inner_index += 1 #move to next row
 		time2totaltime.append(temp_total_time)
 		temp_total_time = []
-			
-		inner_index = 0
-		outer_index += 1
 
 	return time2totaltime
 
 def sum_times(time2totaltime):
+	'''
+	for each raw file read in that has the two columns of time
+	summed up already, sum up the entire column of time2totaltime(list)
+	for each file read in
+	'''
 	sum_of_time = []
 	for time in time2totaltime:
 		sum_of_time.append(np.sum(time))
@@ -68,17 +84,22 @@ def sum_times(time2totaltime):
 	return sum_of_time
 
 def print_sum_of_time(sum_of_time):
-	#print the sum of list
+	'''
+	prints the total sum of the totaltime column for each file
+	'''
 	for i in range(len(sum_of_time)):
 		print('Sum of total time in {0} = {1}s'.format(an_files_only[i], sum_of_time[i]))
 	print('\n')
 
 
 def an_files_to_dict(an_files_only):
+	'''
+	creates a dictionary in which key = filename and the value = dataframe
+	associated with that specific file
+	'''
 	index = 0
 	an_data = dict()
 	temp_df = []
-	#temp_an_list = []
 	for file in an_files_only:
 		an_df = pd.read_csv((directory_string + "/" + an_files_only[index]) , delimiter=" ", names = ['time (s)', 'time (ns)', 'index', 'counts'], header = None)
 		an_df['volts'] = an_df['counts'].apply(adc2counts)
@@ -91,6 +112,10 @@ def an_files_to_dict(an_files_only):
 	return an_data, temp_df
 
 def new_an_dfs(an_df):
+	'''
+	returns a new dataframe for an_files with only
+	two columns --> col0 = total time (s) col1 = counts
+	'''
 	new_an_df = []
 	for df in an_df:
 		new_df = df[['total time (s)' , 'counts']].copy()	
@@ -99,6 +124,11 @@ def new_an_dfs(an_df):
 	return new_an_df
 
 def en_files_to_dict(en_files_only):
+	'''
+	creates a dictionary in which key = filename and the value = dataframe
+	associated with that specific file
+	returns en_dictionary and dataframes(stored in list) for each file
+	'''
 	index = 0
 	en_data = dict()
 	temp_df = []
@@ -113,6 +143,11 @@ def en_files_to_dict(en_files_only):
 	return en_data, temp_df
 
 def dev_names(dev_names_path):
+	'''
+	reads in a text file and finds the device name along 
+	with the energy 
+	returns a dictionary with >> key = device name , value = energy
+	'''
 	dev_names = dict()
 	with open(dev_names_path) as file:
 		for line in file:
@@ -123,6 +158,11 @@ def dev_names(dev_names_path):
 	return dev_names
 
 def manipulate_en_df(en_df):
+	'''
+	creating a new dataframe with only two columns
+	col0 = total time (s)- col1 = energy
+	returns a list with the new dataframe from each en_file 
+	'''
 	en_df_new = []
 	for df in en_df:
 		new_en_df = df[['total time (s)' , 'energy']].copy()	
