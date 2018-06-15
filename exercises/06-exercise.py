50eebc137654618d1cdbbe04ac56431521daf949
Jorge Diaz <jorge7soccer@gmail.com>
2018-06-15 11:59:31 -0400

    DEV: Added documentation.

--- exercises/06-exercise.py
+++ exercises/06-exercise.py
@@ -14,18 +14,31 @@ import pandas as pd
 import os
 
 class ANREADER:
+	'''
+	resource_path and chunk size are arguments passed in
+   	returns a dataframe object where the df is a specified
+   	chunk of data
+	'''
 	def __init__(self, resource_path, chunk_size = 1024):
+		'''
+		adds the chunks of data to a list
+		'''
 		self.chunks_of_data = []
 		for chunk in pd.read_csv(resource_path, chunksize=chunk_size, header=None):
-			self.chunks_of_data.append(chunk)
-			
+			self.chunks_of_data.append(chunk)	
 		pass
 
 	def __call__(self, chunk_num):
+		'''
+		returns specified chunk number/index from list of all chunks created
+		'''
 		result = self.chunks_of_data[chunk_num]
 		return result
 
 	def get_chunks(self):
+		'''
+		returns the number of chunks for specific file
+		'''
 		num_of_chunks = len(self.chunks_of_data)
 		return num_of_chunks
 
@@ -39,6 +52,10 @@ an_files_only = [file for file in files_only if file.startswith("an_")]
 # dataframe is returned 
 
 def file_choice():
+	'''
+	user gets to select which file they want to work with
+	returns number for file choice
+	'''
 	print('Which file would you like to look at?')
 	index = 1
 	for file in an_files_only:
@@ -47,14 +64,23 @@ def file_choice():
 	return int(input("Enter number: "))
 
 def chunk_choice(myreader, file):
+	'''
+	user will need to enter which chunk number is desired from the data
+	returns chunk choice
+	'''
 	print('There are {0} chunks in {1}.'.format( myreader.get_chunks(),  file))
 	return int(input("Which chunk number will you want, choose between 0 - {}:\nChunk choice: ".format(myreader.get_chunks() - 1)))
 
+# subtracting 1 since indices start at 0
 file_choice = file_choice() - 1
 
-myreader = ANREADER(file_path + an_files_only[file_choice - 1], chunk_size=1024)
+# creates object from specified file choice
+myreader = ANREADER(file_path + an_files_only[file_choice], chunk_size=1024)
 
+# this is the users choice for chunk number or row number from data
 chunk_choice = chunk_choice(myreader, an_files_only[file_choice])
 
+# creating a new object that holds dataframe such that the
+# data frame is the specified chunk_number
 newreader = myreader(chunk_choice)
 print(newreader)
