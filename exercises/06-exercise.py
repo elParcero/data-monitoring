6855e28f03b737fc8d8316d0cb263fe79410dd48
Jorge Diaz <jorge7soccer@gmail.com>
2018-06-15 11:49:28 -0400

    ENH: Modified the user functionality.

--- exercises/06-exercise.py
+++ exercises/06-exercise.py
@@ -12,30 +12,23 @@
 '''
 import pandas as pd
 import os
-from Tkinter import *
 
 class ANREADER:
-	def __init__(self, resource_path, file_name, chunk_size = 1024):
+	def __init__(self, resource_path, chunk_size = 1024):
 		self.chunks_of_data = []
-		self.chunks_in_file = dict()
-		
 		for chunk in pd.read_csv(resource_path, chunksize=chunk_size, header=None):
 			self.chunks_of_data.append(chunk)
-			self.chunks_in_file[file_name] = self.chunks_of_data
 			
 		pass
 
-	def __call__(self, file_name, chunk_num):
-		result = self.chunks_in_file[file_name]
-		return result[chunk_num]
+	def __call__(self, chunk_num):
+		result = self.chunks_of_data[chunk_num]
+		return result
 
-	def get_key(self):
-		key = list(self.chunks_in_file.keys())
-		return key[0]
-
-	def get_chunks(self, file_name):
-		num_of_chunks = len(self.chunks_in_file[file_name])
+	def get_chunks(self):
+		num_of_chunks = len(self.chunks_of_data)
 		return num_of_chunks
+
 
 file_path = "/home/jdiaz/projects/data-monitoring/data/iss_sample_data/"
 files_only = next(os.walk(file_path))[2] 
@@ -43,13 +36,25 @@ an_files_only = [file for file in files_only if file.startswith("an_")]
 
 # user will enter file want to work with
 # user will get to enter what chunk number desired
-# dataframe is returned
-file_choice = input('Which file would you like to look at?\n{}\n'.format('	'.join(an_files_only)) ) 
-myreader = ANREADER(file_path + file_choice, file_choice, chunk_size=1024)
+# dataframe is returned 
 
-print('There are {0} chunks in {1}.'.format( myreader.get_chunks(file_choice), myreader.get_key() ))
-chunk_choice = int(input("Which chunk number will you want, choose between 0 - {}:\n".format(myreader.get_chunks(file_choice) - 1)))
+def file_choice():
+	print('Which file would you like to look at?')
+	index = 1
+	for file in an_files_only:
+		print(str(index) + ':' + file)
+		index += 1
+	return int(input("Enter number: "))
 
-newreader = myreader(file_choice.strip(), chunk_choice)
+def chunk_choice(myreader, file):
+	print('There are {0} chunks in {1}.'.format( myreader.get_chunks(),  file))
+	return int(input("Which chunk number will you want, choose between 0 - {}:\nChunk choice: ".format(myreader.get_chunks() - 1)))
 
+file_choice = file_choice() - 1
+
+myreader = ANREADER(file_path + an_files_only[file_choice - 1], chunk_size=1024)
+
+chunk_choice = chunk_choice(myreader, an_files_only[file_choice])
+
+newreader = myreader(chunk_choice)
 print(newreader)
