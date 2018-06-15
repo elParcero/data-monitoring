564f9bb8153acf04d7e33822b164018c29f3b481
Jorge Diaz <jorge7soccer@gmail.com>
2018-06-15 13:19:34 -0400

    ENH: Removed function, updated documentation.

--- exercises/03-exercise.py
+++ exercises/03-exercise.py
@@ -8,14 +8,34 @@ import numpy as np
 
 file_path = "/home/jdiaz/projects/data-monitoring/data/xas_data/xas_test_328.txt"
 
-def get_header(file_path):
+def read_data(file_path):
+	key_value = []
+	info_data = []
+
+	with open(file_path) as file:
+		for line in file:
+			if not line.startswith('#'):
+				info_data.append(line.strip().split())
+	return info_data
+
+
+
+def get_header(file_path, get_data = False):
 	'''
+	Parameters
+	---------------------
+	file_path: (str) the path of the desired file 
+	get_data: bool, optional
+		if true, also returns dataform file. Default is false.
+
 	Open file , reads file in, splits line apart if it starts with #, 
 	one piece is the key, the other piece is the value
 	e.g. # Year: 2018
 	gets stored in a dictionary such that hdr_dict['Year'] = '2018'
-	returns the dictionary hdr_data, and returns a list of all the lines
-	such that the lines do NOT start with # symbol
+	
+	Returns
+	---------------------
+	the dictionary header data and column names if get_data is false, else it returns header data, column names, and file data
 	'''
 	hdr_data = {}
 	key_value = []
@@ -32,28 +52,17 @@ def get_header(file_path):
 						value = key_value[1].replace("\n","").strip()
 						hdr_data[key] = value
 					elif len(key_value) == 1:
-						continue	
+						column_names = line.replace("#","").strip().split()	
+						continue 
+					
 			if not line.startswith("#"):
-				info_data.append(line.strip().split())
+				break
 
-	return hdr_data, info_data
-
-def get_column_names(file_path):
-	'''
-	argument in function is file path
-	this function reads the lines to extract
-	column names and returns them as a list
-	'''
-	column_names = []
-	with open(file_path) as file:
-		for line in file:
-			if line.startswith("#"):
-				if len(line) > 1:
-					key_value = line.split(":", 1)
-					if len(key_value) == 1:
-						column_names = line.replace("#","").strip().split()
-						continue
-	return column_names
+	if(get_data == True):
+		info_data = read_data(file_path)
+		return hdr_data, info_data, column_names
+	else:
+		return hdr_data, column_names
 
 def create_df(info_data, column_names):
 	'''
@@ -93,29 +102,37 @@ def plot_versus_energy(i0_data, it_data, energy_data):
 	plt.show()
 
 #hdr_data is a dictionary | info_data is a list where each element is a line in the file
-hdr_data, info_data = get_header(file_path)
-# geting column names
-column_names = get_column_names(file_path)
+hdr_data = dict()
+column_names = []
 
-# creating a dataframe from the data in the text file
-df = create_df(info_data, column_names)
+print("Header data will be processed.")
+data_desired = int(input("Do you desire to process the data as well?\n1. Yes\n2. No\nChoose Number: "))
+
+if data_desired == 1:
+	hdr_data, info_data , column_names= get_header(file_path, True)
+	df = create_df(info_data, column_names)
+elif data_desired == 2:
+	hdr_data , column_names = get_header(file_path)
+else:
+	print("You entered wrong number. Exiting.")
 
 #printing dictionary and list to make sure values are in place
-print("Header Data")
+print("\nHeader Data")
 print(hdr_data)
 print("\nColumn Names")
 print(column_names)
 
+
 # columns in df object are of Series type, following lines convert 
 # it and i0 and energy COLUMNS to ndarray with float type values
-it_data = np.array(df.it, dtype="float")
-i0_data = np.array(df.i0, dtype="float")
-energy_data = np.array(df.energy, dtype="float")
+if data_desired == 1:
+	plt.ion()
+	it_data = np.array(df.it, dtype="float")
+	i0_data = np.array(df.i0, dtype="float")
+	energy_data = np.array(df.energy, dtype="float")
+	# plotting np.log(i0_data/it_data)
+	plot_log_i0_it(i0_data , it_data)
 
-plt.ion()
+	#plotting versus energy with log
+	plot_versus_energy(i0_data, it_data, energy_data)
 
-# plotting np.log(i0_data/it_data)
-plot_log_i0_it(i0_data , it_data)
-
-#plotting versus energy with log
-plot_versus_energy(i0_data, it_data, energy_data)
