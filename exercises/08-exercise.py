639a5b25f964a2e74279d646afb59adb3d212272
Jorge Diaz <jorge7soccer@gmail.com>
2018-06-21 10:04:04 -0400

    ENH: Added more documentation to the functions.

--- exercises/08-exercise.py
+++ exercises/08-exercise.py
@@ -14,50 +14,92 @@ db = Broker.from_config(temp_config())
 fc = 7.62939453125e-05
 adc2counts = lambda x: ((int(x, 16) >> 8) - 0x40000) * fc \
         if (int(x, 16) >> 8) > 0x1FFFF else (int(x, 16) >> 8)*fc
-enc2counts = lambda x: int(x) if int(x) <= 0 else -(int(x) ^ 0xffffff - 1)
 
 
 class MyHandler(HandlerBase):
     def __init__(self, resource_path, chunk_size=1024):
         '''
-        adds the chunks of data to a list
+        adds the chunks of data to a list for specific file
+
+        Parameters
+        ----------
+        resource_path: str
+            tells the computer where to find the file
+        chunk_size: int (optional)
+            user specifices size of chunk for data, default is 1024
         '''
         self.chunks_of_data = []
         for chunk in pd.read_csv(resource_path, chunksize=chunk_size, 
-        		names =['time (s)', 'time (ns)', 'index', 'counts'], 
-        		delimiter = " ", header=None):
-        	chunk['volts'] = chunk['counts'].apply(adc2counts)
-        	chunk['total time (s)'] = chunk['time (s)'] + 1e-9*chunk['time (ns)']
-        	chunk = chunk.drop(columns = ['time (s)', 'time (ns)', 'counts'])
-        	chunk = chunk[['total time (s)', 'index', 'volts']]
-        	self.chunks_of_data.append(chunk)
+                names =['time (s)', 'time (ns)', 'index', 'counts'], 
+                delimiter = " ", header=None):
+            chunk['volts'] = chunk['counts'].apply(adc2counts)
+            chunk['total time (s)'] = chunk['time (s)'] + 1e-9*chunk['time (ns)']
+            chunk = chunk.drop(columns = ['time (s)', 'time (ns)', 'counts'])
+            chunk = chunk[['total time (s)', 'index', 'volts']]
+            self.chunks_of_data.append(chunk)
 
     def __call__(self, chunk_num):
         '''
-        returns specified chunk number/index from list of all chunks created
+        Returns 
+        -------
+        result: dataframe object
+            specified chunk number/index from list of all chunks created
         '''
         result = self.chunks_of_data[chunk_num]
         return result
 
 
 def get_resource(resource_uid, filepath):
-	resource = {'id': resource_uid,
- 	'path_semantics': 'posix',
- 	'resource_path': filepath,
- 	'root': '/home/jdiaz',
- 	'resource_kwargs': {},
- 	'spec': 'PIZZABOX_AN_FILE_TXT',
- 	'uid': resource_uid,
-	}
-	return resource
+    '''
+    returns a resource document for file
+
+    Parameters
+    ----------
+    resource_uid: str
+        resource_uid will be a unique identifier for the resource document that 
+        will be created
+    filepath: str
+        path to file
+    
+    Returns
+    -------
+    resource: dict
+        dictionary that contains specific key,val arguments that relates to file
+    '''
+    resource = {'id': resource_uid,
+    'path_semantics': 'posix',
+    'resource_path': filepath,
+    'root': '/home/jdiaz',
+    'resource_kwargs': {},
+    'spec': 'PIZZABOX_AN_FILE_TXT',
+    'uid': resource_uid,
+    }
+    return resource
 
 
 def get_datum(datum_uid, resource_uid):
-	datum = {'datum_id': datum_uid,
- 	'datum_kwargs': {'chunk_num' : 7},
- 	'resource': resource_uid
- 	}
-	return datum
+    '''
+    returns a datum document for file
+
+    Parameters
+    ----------
+    datum_uid: str
+        datum_uid will be a unique identifier for the datum document that 
+        will be created
+    resource_uid: str
+        the same resource uid for the file being worked with, datum doc will be able to 
+        point to resource document
+
+    Returns
+    -------
+    datum: dict
+        dictionary that contains specific key,val arguments that relates to file
+    '''
+    datum = {'datum_id': datum_uid,
+    'datum_kwargs': {'chunk_num' : 7},
+    'resource': resource_uid
+    }
+    return datum
 
 
 filepath = "/home/jdiaz/projects/data-monitoring/data/iss_sample_data/an_2f218f"
@@ -69,7 +111,7 @@ datum = get_datum(datum_uid, resource['uid']) # retrieving dict for datum
 
 # registering a handler according to a certain spec
 new_resource = db.reg.insert_resource( "PIZZABOX_AN_FILE_TXT", resource_path = resource['resource_path'], \
-	resource_kwargs = resource['resource_kwargs'], uid = resource['uid'])
+    resource_kwargs = resource['resource_kwargs'], uid = resource['uid'])
 
 new_datum = db.reg.insert_datum(resource = new_resource, datum_id = datum['datum_id'],\
  datum_kwargs = datum['datum_kwargs'])
@@ -83,7 +125,7 @@ datums = list(datum_gen) # converts generator object into list
 
 # creating object of MyHandler class
 fh = MyHandler(resource_path=resource['resource_path'],
-	**resource['resource_kwargs'])
+    **resource['resource_kwargs'])
 # we want the first datum 
 datum = datums[0]
 # this is the data retrieved from the specificed 'datum_kwargs'
