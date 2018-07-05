abea71cd33c83be00d08d63b8d4ced464270bfb3
Jorge Diaz <jorge7soccer@gmail.com>
2018-07-05 11:59:33 -0400

    ENH: Saving CHX file sizes to .dat file

--- exercises/0002_eiger_handler.py
+++ exercises/0002_eiger_handler.py
@@ -1,10 +1,12 @@
 from databroker import Broker
 import pandas as pd
-import numpy as np
-import time 
+import time
 import datetime
+from time import mktime
+from eiger_io.fs_handler import EigerHandler
 
 def file_sizes(hdrs, db):
+
     unique_resources = set()
     time_size = dict()
     file_sizes = list()
@@ -34,44 +36,42 @@ def file_sizes(hdrs, db):
                                         unique_resources.add(resource_id)
                                     datum_gen = db.reg.datum_gen_given_resource(resource_id)
                                     datum_kwargs = [datum['datum_kwargs'] for datum in datum_gen]
-                                    #timestamp = time.ctime(event['time'])
                                     timestamp = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(int(event['time'])))
-                                    
+                                    timestamp = time.strptime(timestamp, '%Y-%m-%d %H:%M:%S')
+                                    timestamp = datetime.datetime.fromtimestamp(mktime(timestamp))
                                     # get the file handler using this
                                     fh = db.reg.get_spec_handler(resource_id)
                                     print(fh)
-                                    file_sizes = fh.get_file_size(datum_kwargs)
+                                    file_sizes = sum(fh.get_file_size(datum_kwargs))
                                     print(file_sizes)
-                                    if not file_sizes:
-                                        file_sizes.append(0)
-                                        time_size[timestamp] = file_sizes
-                                    else:
-                                        time_size[timestamp] = file_sizes
+                                    time_size[timestamp] = file_sizes
                 except StopIteration:
                     break
-                #except KeyError:
-                    #continue
+#               except KeyError:
+#                   continue
     end_time = time.time()
     total_time = end_time - start_time
     print(total_time)
-    #return file_sizes
     return time_size
 
 
 db = Broker.named("chx")
-
-from eiger_io.fs_handler import EigerHandler
-
 db.reg.register_handler("AD_EIGER", EigerHandler)
 db.reg.register_handler("AD_EIGER2", EigerHandler)
 
-hdrs = iter(db(since="2016-08-19", until="2016-08-22", plan_name='count'))
+hdrs = iter(db(start_time="2016-08-19", stop_time="2016-08-22", plan_name='count'))
 
 # first two are bad, so I'm skipping them
 hdr = next(hdrs)
 hdr = next(hdrs)
 
 # good one
-#hdr = next(hdrs)
+# hdr = next(hdrs)
 file_sizes = file_sizes(hdrs, db)
 
+# make dataframe
+df = pd.DataFrame.from_dict(file_sizes, orient='index')
+df.columns = ['file_size']
+df.index.name = 'timestamp'
+
+df.to_csv('chx_file_sizes.dat', sep=" ")
