2b72d1af979d41655e42926674e769ff9c86c972
Jorge Diaz <jdiaz@xf11id-ws2.cs.nsls2.local>
2018-07-06 16:58:47 -0400

    DEV: Different plans for CHX beamline (file sizes).

---
+++ exercises/0003_eiger_handler_chx.py
@@ -0,0 +1,107 @@
+#0003
+from databroker import Broker
+import pandas as pd
+import os
+
+import datetime
+import time
+from time import mktime
+
+from eiger_io.fs_handler import EigerHandler
+from databroker.assets.handlers import AreaDetectorTiffHandler
+
+def file_sizes(hdrs, db):
+
+    unique_resources = set()
+    time_size = dict()
+    file_sizes = list()
+    FILESTORE_KEY = "FILESTORE:"
+    start_time = time.time()
+    timestamp = 0.0
+    for hdr in hdrs:
+        for stream_name in hdr.stream_names:
+            events = hdr.events(stream_name=stream_name)
+            events = iter(events)
+            while True:
+                try:
+                    event = next(events)
+                    if "filled" in event:
+                        # there are keys that may not be filled
+                        for key, val in event['filled'].items():
+                            # if this is true, then we have a datum
+                            if not val:
+                                # get the datum
+                                if key in event['data']:
+                                    datum_id = event['data'][key]
+                                    resource = (db.reg.resource_given_datum_id(datum_id))
+                                    resource_id = resource['uid']
+                                    if resource_id in unique_resources:
+                                        continue
+                                    else:
+                                        unique_resources.add(resource_id)
+                                    datum_gen = db.reg.datum_gen_given_resource(resource_id)
+                                    datum_kwargs = [datum['datum_kwargs'] for datum in datum_gen]
+                                    timestamp = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(int(event['time'])))
+                                    timestamp = time.strptime(timestamp, '%Y-%m-%d %H:%M:%S')
+                                    timestamp = datetime.datetime.fromtimestamp(mktime(timestamp))
+                                    # get the file handler using this
+                                    fh = db.reg.get_spec_handler(resource_id)
+                                    print(fh)
+                                    file_list = fh.get_file_list(datum_kwargs)
+                                    file_size = get_file_size(file_list)
+                                    print(file_size)
+                                    time_size[timestamp] = file_size
+                except StopIteration:
+#                   continue
+    end_time = time.time()
+    total_time = end_time - start_time
+    print(total_time)
+    return time_size
+
+def get_file_size(file_list):
+    sizes = []
+    for file in file_list:
+        if os.path.isfile(file):
+            sizes.append(os.path.getsize(file))
+    return sum(sizes)
+
+db = Broker.named("chx")
+db.reg.register_handler("AD_EIGER", EigerHandler)
+db.reg.register_handler("AD_EIGER2", EigerHandler)
+db.reg.register_handler("AD_TIFF", AreaDetectorTiffHandler)
+
+plan_names = ['count', 'scan', 'rel_scan']
+hdrs = []
+for plan in plan_names:
+    hdrs.append(iter(db(start_time="2015-01-01", stop_time="2017-10-21", plan_name = plan)))
+
+# first two are bad, so I'm skipping them
+
+# good one
+# hdr = next(hdrs)
+f_sizes = []
+for i in range(3):
+    f_sizes.append(file_sizes(hdrs[i], db))
+
+def make_dfs(file_sizes):
+    dfs = []
+    for file in file_sizes:
+        df = pd.DataFrame.from_dict(file, orient='index')
+        df.columns = ['file_size']
+        df.index.name = 'timestamp'
+        dfs.append(df)
+    return dfs
+
+dfs = make_dfs(f_sizes)
+
+
+'''    
+# make dataframe
+df = pd.DataFrame.from_dict(file_sizes_, orient='index')
+df.columns = ['file_size']
+df.index.name = 'timestamp'
+
+df.to_csv('chx_file_sizes.dat', sep=" ")
+'''
+
+
