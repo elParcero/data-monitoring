d8cdff33b8e3eb97cfdfd74ba8d139a61468d9a6
Jorge Diaz <jorge7soccer@gmail.com>
2018-07-09 13:33:04 -0400

    ENH: Fixing syntax.

--- exercises/beamline_plotting_v2.py
+++ exercises/beamline_plotting_v2.py
@@ -2,8 +2,8 @@
 Author: Jorge Diaz Jr
 '''
 import os
-from os.path import basename
-from pathlib import Path
+
+
 from datetime import datetime
 import pandas as pd
 
@@ -11,12 +11,11 @@ import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 from cycler import cycler
 
-import numpy as np
-
 years = mdates.YearLocator()   # every year
 months = mdates.MonthLocator()  # every month
 yearsFmt = mdates.DateFormatter('%Y')
 plt.ion()
+
 
 def readin_file(files, file_path, parent_files):
     index = 0
@@ -27,34 +26,35 @@ def readin_file(files, file_path, parent_files):
 
     for i in range(len(files)):
         for j in range(len(files[parent_files[i]])):
-            with open(file_path + parent_files[i] + "/" + files[parent_files[i]][j] ) as file:
+            with open(file_path + parent_files[i] +
+                      "/" + files[parent_files[i]][j]) as file:
                 for line in file:
                     if index == 0:
                         index += 1
                         continue
                     else:
                         key_val = line.split(" ")
-                        datetime_obj = datetime.strptime(key_val[0].strip(), date_format)
+                        datetime_obj = datetime.strptime(key_val[0].strip(),
+                                                         date_format)
                         oreo[datetime_obj] = float(key_val[1])
-            key_name = parent_files[i].upper().split('-',1)[0]
-            det_name = files[parent_files[i]][j].replace(".dat","").upper()
+            key_name = parent_files[i].upper().split('-', 1)[0]
+            det_name = files[parent_files[i]][j].replace(".dat", "").upper()
             det_names.append(det_name)
-            date_size[key_name+':'+det_name] = oreo
+            date_size[key_name + ':' + det_name] = oreo
             oreo = {}
             index = 0
 
-    return date_size , det_names
+    return date_size, det_names
 
 
 def create_df(file_size, det_names):
     dfs = dict()
-    dfs_lst = []
     for key in file_size:
         df = pd.DataFrame.from_dict(file_size[key], orient='index')
         df.index.name = 'date'
-        df.columns = [key.split(':',1)[1]]
-        main_key = key.split(':',1)[0]
-        if not main_key in dfs:
+        df.columns = [key.split(':', 1)[1]]
+        main_key = key.split(':', 1)[0]
+        if main_key not in dfs:
             dfs[main_key] = []
         dfs[main_key].append(df)
 
@@ -64,19 +64,15 @@ def create_df(file_size, det_names):
 def plot_usage_versus_day(newdfs, file_names):
     plt.clf()
     color_cycle = cycler(color=['r', 'g', 'b'])
-    index = 0
-    beamline = [file.upper().split('-',1)[0] for file in file_names]
+    beamline = [file.upper().split('-', 1)[0] for file in file_names]
 
     newdfs = [newdf.cumsum() for newdf in newdfs]
     for i in range(len(newdfs)):
-        fig, ax= plt.subplots()
-        print(i)
-        for column,color in zip(newdfs[i].columns, color_cycle):
+        fig, ax = plt.subplots()
+        for column, color in zip(newdfs[i].columns, color_cycle):
             df = newdfs[i][column]
-            plt.bar( df.index, df * 1e-9 ,
-                        width = 1, alpha = 0.6,
-                        color = color['color'],
-                        label = column)
+            plt.bar(df.index, df * 1e-9, width=1, alpha=0.6,
+                    color=color['color'], label=column)
 
             ax.set_xlabel('Time (daily)')
             ax.set_ylabel('Usage (GB)')
@@ -85,14 +81,15 @@ def plot_usage_versus_day(newdfs, file_names):
             ax.xaxis.set_major_formatter(yearsFmt)
             ax.xaxis.set_minor_locator(months)
             plt.show()
-            plt.legend(loc = 2)
+            plt.legend(loc=2)
             plt.savefig(beamline[i] + "_daily_cumulative.png")
+
 
 def plot_usage_versus_week(newdfs, file_names):
     plt.clf()
     color_cycle = cycler(color=['r', 'g', 'b'])
     index = 0
-    beamline = [file.upper().split('-',1)[0] for file in file_names]
+    beamline = [file.upper().split('-', 1)[0] for file in file_names]
 
     newdfs = [newdf.resample('W').sum() for newdf in newdfs]
     newdfs = [newdf.cumsum() for newdf in newdfs]
@@ -100,10 +97,9 @@ def plot_usage_versus_week(newdfs, file_names):
     for i in range(len(newdfs)):
         fig, ax= plt.subplots()
         print(i)
-        for column,color in zip(newdfs[i].columns, color_cycle):
+        for column, color in zip(newdfs[i].columns, color_cycle):
             df = newdfs[i][column]
-            plt.bar( df.index, df * 1e-9 ,
-                        width = 7, alpha = 0.6,
+            plt.bar(df.index, df * 1e-9, width=7,alpha= 0.6,
                         color = color['color'],
                         label = column)
 
