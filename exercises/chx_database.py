4855bcdca7bace9e7c52a319937632dafa8500fd
Jorge Diaz <jorge7soccer@gmail.com>
2018-07-11 16:57:09 -0400

    ENH: Creating database table.

--- exercises/chx_database.py
+++ exercises/chx_database.py
@@ -1,2 +1,8 @@
 import sqlite3
+import dataset
+
 conn = sqlite3.connect('beamlines.db')
+db = dataset.connect("sqlite:///database.db")
+beamline_name = 'chx'
+table = db[beamline_name]
+table.insert(dict(time="2017-01-01", file_size=100, plan_name="count", detector_name="eiger4m_single"))
