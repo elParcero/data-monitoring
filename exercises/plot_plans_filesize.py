8aa3929f6b9583d031e7e3c02c1f5b35cd2934b0
Jorge Diaz <jorge7soccer@gmail.com>
2018-07-11 13:20:02 -0400

    ENH: Calculated hourly sum from 2015-01-01 to 2018-12-31.

--- exercises/plot_plans_filesize.py
+++ exercises/plot_plans_filesize.py
@@ -6,6 +6,7 @@ import pandas as pd
 
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
+import matplotlib.ticker as ticker
 
 plt.ion()
 
@@ -17,17 +18,6 @@ def readin_files(file_path, dat_files):
         df.index = pd.to_datetime(df.pop('timestamp'))
         data[file.replace('.dat', '').replace('_FILESIZE_V2', '').upper()] = df
     return data
-
-
-file_path = '/home/jdiaz/projects/data-monitoring/exercises/plan_plots'
-dat_files = [dat_file for dat_file in os.listdir(file_path)
-             if dat_file.endswith('.dat')]
-
-# files read in and saved as dataframes
-data = readin_files(file_path, dat_files)
-
-# for key, dat in data.items():
-#    dat.to_csv('{}_V2.dat'.format(key), sep=" ")
 
 
 def plot_usage(data):
@@ -43,7 +33,7 @@ def plot_usage(data):
 #        plt.bar(dat.index, dat[col_name] * 1e-9, width=7,
 #               label=col_name.upper(), color='navy')
         plt.plot(dat.index, dat[col_name] * 1e-9,
-               label=col_name.upper(), color='navy')
+                 label=col_name.upper(), color='navy')
         ax.set_xlabel('Time (daily)')
         ax.set_ylabel('Usage (GB)')
         ax.set_title(key)
@@ -53,7 +43,65 @@ def plot_usage(data):
         ax.xaxis.set_minor_locator(mdates.DayLocator())
         plt.show()
         plt.legend(loc=2)
-        plt.savefig(key + '.png')
+#        plt.savefig(key + '.png')
 
 
-plot_usage(data)
+def hourly_sum(data):
+    hourly_sum = dict()
+    for key, dat in data.items():
+        dat = dat.resample('H').sum()
+        dat = dat.groupby(dat.index.hour).sum()
+        hourly_sum[key] = dat
+    return hourly_sum
+
+
+def line_plot_hourly_sum(hourly_sum):
+    labels = range(24)
+    for key, data in hourly_sum.items():
+        col_name = data.columns.values[0]
+        fig, ax = plt.subplots()
+        plt.plot(data.index, data[col_name] * 1e-9,
+                 label=col_name.upper(), color='navy')
+        ax.set_xlabel('Time (daily)')
+        ax.set_ylabel('Usage (GB)')
+        ax.set_title(key)
+        ax.xaxis.set_major_locator(ticker.FixedLocator(labels))
+        plt.show()
+        plt.legend(loc=2)
+        plt.savefig('{}_line_hourly.png'.format(key))
+
+
+def bar_plot_hourly_sum(hourly_sum):
+    labels = range(24)
+    for key, data in hourly_sum.items():
+        col_name = data.columns.values[0]
+        fig, ax = plt.subplots()
+        plt.bar(data.index, data[col_name] * 1e-9, width=0.5,
+                label=col_name.upper(), color='navy')
+        ax.set_xlabel('Time (daily)')
+        ax.set_ylabel('Usage (GB)')
+        ax.set_title(key)
+        ax.xaxis.set_major_locator(ticker.FixedLocator(labels))
+        plt.show()
+        plt.legend(loc=2)
+        plt.savefig('{}_bar_hourly.png'.format(key))
+
+
+file_path = '/home/jdiaz/projects/data-monitoring/exercises/plan_plots'
+dat_files = [dat_file for dat_file in os.listdir(file_path)
+             if dat_file.endswith('.dat')]
+
+# files read in and saved as dataframes
+data = readin_files(file_path, dat_files)
+
+# for key, dat in data.items():
+#    dat.to_csv('{}_V2.dat'.format(key), sep=" ")
+
+# plot_usage(data)
+hourly_sum = hourly_sum(data)
+for key, data in hourly_sum.items():
+    print(key)
+    data.to_csv('{}_hourly_sum.dat'.format(key.lower()), sep=' ')
+
+# line_plot_hourly_sum(hourly_sum)
+# bar_plot_hourly_sum(hourly_sum)
