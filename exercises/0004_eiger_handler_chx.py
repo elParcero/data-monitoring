0a2784811d7d6d86c40571aa68572d84c734f39a
Jorge Diaz <jorge7soccer@gmail.com>
2018-07-13 13:25:13 -0400

    ENH: Finding file size for plan and detector.

--- exercises/0004_eiger_handler_chx.py
+++ exercises/0004_eiger_handler_chx.py
@@ -29,33 +29,34 @@ def file_sizes(hdrs, db, detector):
                             # if this is true, then we have a datum
                                 if not val:
                                     # get the datum
-                                    if key in event['data'] and key == detector:
-                                        print('{} = {}'.format(key, detector))
-                                        datum_id = event['data'][key]
-                                        resource = (db.reg.resource_given_datum_id(datum_id))
-                                        resource_id = resource['uid']
-                                        if resource_id in unique_resources:
-                                            continue
-                                        else:
-                                            unique_resources.add(resource_id)
-                                        datum_gen = db.reg.datum_gen_given_resource(resource_id)
-                                        try:
-                                            datum_kwargs = [datum['datum_kwargs'] for datum in datum_gen]
-                                        except TypeError:
-                                            print('TypeError ... ignore ...')
-                                        timestamp = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(int(event['time'])))
-                                        timestamp = time.strptime(timestamp, '%Y-%m-%d %H:%M:%S')
-                                        timestamp = datetime.datetime.fromtimestamp(mktime(timestamp))
-                                        # get the file handler using this
-                                        fh = db.reg.get_spec_handler(resource_id)
-                                        print(fh)
-                                        try:
-                                            file_list = fh.get_file_list(datum_kwargs)
-                                            file_size = get_file_size(file_list)
-                                        except KeyError:
-                                            file_size = 0
-                                        print(file_size)
-                                        time_size[timestamp] = file_size
+                                    if key in event['data']:
+                                        if key == detector:
+                                            print('{} = {}'.format(key, detector))
+                                            datum_id = event['data'][key]
+                                            resource = (db.reg.resource_given_datum_id(datum_id))
+                                            resource_id = resource['uid']
+                                            if resource_id in unique_resources:
+                                                continue
+                                            else:
+                                                unique_resources.add(resource_id)
+                                            datum_gen = db.reg.datum_gen_given_resource(resource_id)
+                                            try:
+                                                datum_kwargs = [datum['datum_kwargs'] for datum in datum_gen]
+                                            except TypeError:
+                                                print('TypeError ... ignore ...')
+                                            timestamp = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(int(event['time'])))
+                                            timestamp = time.strptime(timestamp, '%Y-%m-%d %H:%M:%S')
+                                            timestamp = datetime.datetime.fromtimestamp(mktime(timestamp))
+                                            # get the file handler using this
+                                            fh = db.reg.get_spec_handler(resource_id)
+                                            print(fh)
+                                            try:
+                                                file_list = fh.get_file_list(datum_kwargs)
+                                                file_size = get_file_size(file_list)
+                                            except KeyError:
+                                                file_size = 0
+                                            print(file_size)
+                                            time_size[timestamp] = file_size
                 except StopIteration:
                     break
                 except KeyError:
@@ -84,7 +85,10 @@ db.reg.register_handler("AD_TIFF", AreaDetectorTiffHandler)
 plan_names = ['count', 'scan', 'rel_scan']
 detector_names = ['eiger1m_single_image','eiger4m_single_image','xray_eye2_image']
 
-hdrs = iter(db(since='2018-04-27', until='2018-12-31', plan_name=plan_names[0]))
-
-file_sizes(hdrs, db, detector_names[0])
+det_size = dict()
+#hdrs = iter(db(since='2018-04-27', until='2018-12-31', plan_name=plan_names[0]))
+for plan in plan_names:
+    for detector in detector_names:
+        hdrs = iter(db(since='2017-04-27', until='2018-12-31', plan_name=plan))
+        det_size['{}:{}'.format(plan, detector)] =file_sizes(hdrs, db, detector)
 
