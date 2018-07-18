6d0a537164cdfb777d7796a375348b6790519808
Jorge Diaz <jorge7soccer@gmail.com>
2018-07-18 14:36:20 -0400

    ENH: Plotting histogram for CHX plans.

--- exercises/0008_chx_hist_plots.py
+++ exercises/0008_chx_hist_plots.py
@@ -1,17 +1,34 @@
 import os
 import pandas as pd
+import numpy as np
 
 import matplotlib.pyplot as plt
 
+plt.ion()
 
 def create_dfs(file_path, files):
-    dfs
+    dfs = []
     for file in files:
-        df = pd.read_csv(file_path + '/' + file, sep='')
+        df = pd.read_csv(file_path + '/' + file, sep=' ')
         df.index = pd.to_datetime(df.pop('timestamp'))
         dfs.append(df)
     return dfs
 
+def plot_histogram(dfs):
+    for df in dfs:
+        fig, ax = plt.subplots()
+        col_name = df.columns.values[0]
+        x = df[col_name].values
+        plt.hist(x, bins = 20)
+        ax.set_title(col_name)
+        plt.gca().set_xlim(x.min(), x.max())
+        plt.show()
+        plt.savefig('chx_{}'.format(col_name.replace('(','_').replace(')','')))
+
+
 file_path = '/home/jdiaz/projects/data-monitoring/exercises/plan_plots'
 files = [file for file in os.listdir(file_path) if file.endswith('.dat')]
 
+dfs = create_dfs(file_path, files)
+
+plot_histogram(dfs)
