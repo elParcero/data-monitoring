9bb993f81fdb48b8945c2578c096ca47673a0727
Jorge Diaz <jorge7soccer@gmail.com>
2018-07-18 14:51:54 -0400

    ENH: Added another file path.

--- exercises/0008_chx_hist_plots.py
+++ exercises/0008_chx_hist_plots.py
@@ -9,7 +9,7 @@ plt.ion()
 def create_dfs(file_path, files):
     dfs = []
     for file in files:
-        df = pd.read_csv(file_path + '/' + file, sep=' ')
+        df = pd.read_csv(file_path + '/' + file, sep=',')
         df.index = pd.to_datetime(df.pop('timestamp'))
         dfs.append(df)
     return dfs
@@ -20,13 +20,14 @@ def plot_histogram(dfs):
         col_name = df.columns.values[0]
         x = df[col_name].values
         plt.hist(x, bins = 20)
-        ax.set_title(col_name)
+        ax.set_title(col_name.replace('(fileusage)',''))
         plt.gca().set_xlim(x.min(), x.max())
         plt.show()
-        plt.savefig('chx_{}'.format(col_name.replace('(','_').replace(')','')))
+        plt.savefig('chx_{}'.format(col_name.replace('(fileusage)','').replace(':','_')))
 
 
-file_path = '/home/jdiaz/projects/data-monitoring/exercises/plan_plots'
+#file_path = '/home/jdiaz/projects/data-monitoring/exercises/plan_plots'
+file_path = '/home/jdiaz/projects/data-monitoring/exercises/plans_dets_fsize'
 files = [file for file in os.listdir(file_path) if file.endswith('.dat')]
 
 dfs = create_dfs(file_path, files)
