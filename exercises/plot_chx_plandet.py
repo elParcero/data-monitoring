9b69163bb471c028758d9e9365c01d1937bda62e
Jorge Diaz <jorge7soccer@gmail.com>
2018-07-26 11:49:28 -0400

    ENH: Added function to plot detectors/plans regularly.

--- exercises/plot_chx_plandet.py
+++ exercises/plot_chx_plandet.py
@@ -43,9 +43,27 @@ def plot_matrix(dfs):
 
     #for df in dfs:
 
+def plot_dets(dfs):
+    plt.ion()
+
+    for df in dfs:
+        df = df.resample('D').sum()
+        fig, ax = plt.subplots()
+        col_name = df.columns.values[0]
+        plt.bar(df.index, df[col_name] * 1e-9, width=2.0)
+        fig.autofmt_xdate(bottom=0.2, rotation=57, ha='right')
+        ax.xaxis.set_major_locator(mdates.MonthLocator())
+        ax.xaxis.set_major_formatter(mdates.DateFormatter('%b %y'))
+        ax.xaxis.set_minor_locator(mdates.DayLocator())
+        ax.set_ylabel('File usage (GB)')
+        ax.set_xlabel('Daily Sum')
+        ax.set_title(col_name.replace('(fileusage)','').replace(':',': ').upper())
+    plt.show()
+
 
 file_path = '/home/jdiaz/projects/data-monitoring/exercises/plans_dets_fsize'
 files = [file for file in os.listdir(file_path) if file.endswith('.dat')]
 dfs = create_dfs(file_path, files)
 
-plot_matrix(dfs)
+#plot_matrix(dfs)
+plot_dets(dfs)
