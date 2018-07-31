cb4bce64c85d03d61a1fbd4b2dd7dc0c76d9f9cd
Jorge Diaz <jorge7soccer@gmail.com>
2018-07-31 14:22:51 -0400

    ENH: Plotting histograms for CHX plans.

--- exercises/chx_2_histogram.py
+++ exercises/chx_2_histogram.py
@@ -20,7 +20,7 @@ def readin_files(file_path, files):
         file_usage[col_name.replace('(','_').replace(')','')] = arr
     return file_usage
 
-def histogram_plot(file_usage):
+def histogram_plot(file_usage, log_yaxis=False):
     min_size = 0
     max_size = 1e9 # 10 GB here
     Nbins = 10000
@@ -32,6 +32,8 @@ def histogram_plot(file_usage):
         ax.set_xlabel('FILE USAGE')
         ax.set_ylabel('FREQUENCY')
         ax.set_title('CHX | Plan:Detector')
+        if log_yaxis:
+            ax.set_yscale('log')
         plt.plot(h.centers[0] *1e-6, h.values, label=plan)
         plt.legend()
         plt.savefig('hist_{}'.format(plan))
@@ -42,4 +44,4 @@ files = [file for file in os.listdir(file_path) if file.endswith('.dat')]
 
 file_usage = readin_files(file_path, files)
 
-histogram_plot(file_usage)
+histogram_plot(file_usage, True)
